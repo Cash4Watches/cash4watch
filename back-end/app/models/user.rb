d1351d475b7ac10b8883dc6bd3d61edf63457242
mohamed muflahi <muflahimohamed88@gmail.com>
2022-07-01 19:32:48 -0400

    added Active Storage Bucket

--- back-end/app/models/user.rb
+++ back-end/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :orders
   has_secure_password
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
 end
