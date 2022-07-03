825c3f349be89472045fa2a922c7676accad708e
mohamed muflahi <muflahimohamed88@gmail.com>
2022-07-03 18:14:02 -0400

    login added

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
-    secret_key = Rails.application.credentials.dig(:jwt_key)
+    
+    # secret_key = Rails.application.credentials.dig(:jwt_key)
+    def secret_key
+        Rails.application.credentials.dig(:jwt_key)
+    end
     def encode(payload)
         JWT.encode(payload, secret_key, 'HS256')
     end
@@ -8,7 +12,7 @@ class ApplicationController < ActionController::Base
         JWT.decode(token, secret_key, true, { algorithm: 'HS256'})[0]
     end
     def create
-        user = User.find_by(username: params[:username])
+        user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
             # session[:user_id] = user.id
             payload = {'user_id': user.id}
@@ -16,7 +20,8 @@ class ApplicationController < ActionController::Base
             # result = serialize(user, UserSerializer).
             # merge(serialize(token, UserSerializer)) 
             # include: { posts: {include: { comments: {only: :body } },only: :title } }
-            render json: {user: user, token: token}
+            x = user.as_json(only: [:id, :full_name,:email,:company,:street1,:street2,:city,:state,:zip,:phone])
+            render json: {user: x, token: token}
             
         else 
             render json: {
--- back-end/app/serializers/user_serializer.rb
+++ back-end/app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :full_name,:password,:email,:company,:street1,:street2,:city,:state,:zip,:phone
+  attributes :id, :full_name,:email,:company,:street1,:street2,:city,:state,:zip,:phone
 end
