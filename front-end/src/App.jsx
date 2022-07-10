39ab5c54f6a77f7da79f2b4676a16fad7a03804b
mustafabin <mbin828@gmail.com>
2022-07-10 12:49:45 -0400

    dashboard paths and files setup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./screens/Landing.jsx";
 import Navbar from "./components/Nav.jsx";
 import Footer from "./components/Footer.jsx";
+import Dashboard from "./components/Dashboard.jsx";
 import Form from "./screens/Form.jsx";
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/form" element={<Form />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route
           path="*"
           element={
---
+++ front-end/src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import "../styles/Dashboard.scss";
+
+function Dashboard() {
+  return (
+    <>
+      <div className="Dashboard">
+        <h1>Dashboard</h1>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
