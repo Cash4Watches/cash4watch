3a9e9deb5b02780466c1a1b36a71cdf25facaeb8
mohamed muflahi <muflahimohamed88@gmail.com>
2022-07-10 11:21:13 -0400

    switch case

--- back-end/app/controllers/steps_controller.rb
+++ back-end/app/controllers/steps_controller.rb
@@ -4,6 +4,18 @@ class StepsController < ApplicationController
         if step
             step.completed = !step.completed
             step.save!
+            case step.index
+            when 1
+                # email for step 1
+            when 2
+                # email for step 2
+            when 3
+                # email for step 3
+            when 4
+                # email for step 4
+            when 5
+                # email for step 5
+            end
             render json: step
         else
             render json: {message: "Failed to find step with id"}
