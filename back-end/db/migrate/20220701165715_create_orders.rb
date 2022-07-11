b5f9efb49342b172ba3148625de44e755a6be178
Mohamed Muflahi <muflahi@Mohameds-MacBook-Pro.local>
2022-07-11 14:26:35 -0400

    herkou push

--- back-end/db/migrate/20220701165715_create_orders.rb
+++ back-end/db/migrate/20220701165715_create_orders.rb
@@ -3,8 +3,8 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders, id: :uuid do |t|
       t.string :brand_name
-      t.integer :model_number
-      t.integer :reference_number
+      t.string :model_number
+      t.string :reference_number
       t.string :condition
       t.string :previous_service
       t.string :previous_polish
