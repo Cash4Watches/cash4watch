1a09156c2b5bcb5ab1844960e9dc32df48c71f6d
mustafabin <mbin828@gmail.com>
2022-07-20 18:14:30 -0400

    removed red underline added few cosmetic changes to returning landing page

--- front-end/src/components/LandingBox.jsx
+++ front-end/src/components/LandingBox.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import AddIcon from "@mui/icons-material/Add";
 
 export default function LandingBox() {
   let navigate = useNavigate();
@@ -20,11 +21,14 @@ export default function LandingBox() {
         </div>
         <div className="LandingBox-content">
           <div className="LandingBox-content-text">
-            <h1>Welcome Back ! </h1>
-            <span>{abbreviateName()}</span>
+            <h1>
+              Hey, <span>{abbreviateName()}</span>
+            </h1>
           </div>
           <button onClick={() => navigate("/dashboard")}>My Dashboard</button>
-          <button onClick={() => navigate("/form")}>New Order</button>
+          <button onClick={() => navigate("/form")}>
+            New Order <AddIcon fontSize="large" />
+          </button>
         </div>
       </div>
     </>
