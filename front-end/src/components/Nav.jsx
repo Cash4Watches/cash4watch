34b00155bdde82336a38e11552adb4d616776fdb
mustafabin <mbin828@gmail.com>
2022-08-01 13:46:14 -0400

    market carasol  v1

--- front-end/src/components/Nav.jsx
+++ front-end/src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import "../styles/Nav.scss";
 import logo from "../media/logo.svg";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import AccountCircleTwoToneIcon from "@mui/icons-material/AccountCircleTwoTone";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
@@ -8,11 +8,50 @@ import { clearUser } from "../state/user.js";
 import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
 
 function Navbar() {
+  let fakeMarketValues = [
+    {
+      name: "Rolex",
+      price: "10000",
+    },
+    {
+      name: "Cartier",
+      price: "69420",
+    },
+    {
+      name: "Omega",
+      price: "9999",
+    },
+    {
+      name: "Bulgari",
+      price: "1245",
+    },
+    {
+      name: "Bulgari",
+      price: "1245",
+    },
+    {
+      name: "Rolex",
+      price: "1245",
+    },
+    {
+      name: "Rolex",
+      price: "1245",
+    },
+    {
+      name: "Rolex2",
+      price: "1245",
+    },
+    {
+      name: "Rolex3",
+      price: "1245",
+    },
+  ];
+  const marketRef = useRef(null);
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
-
   let navigate = useNavigate();
   const [navbutton, setNavbutton] = useState();
+  const [market, setMarket] = useState(fakeMarketValues);
   useEffect(() => {
     let handleIcon = () => {
       if (800 <= window.innerWidth) {
@@ -43,6 +82,19 @@ function Navbar() {
       window.removeEventListener("resize", handleIcon);
     };
   }, [user]);
+  useEffect(() => {
+    if (market.length > 0) {
+      let shiftAmount = marketRef.current.offsetWidth / 2 + 300;
+      document.body.style.setProperty(
+        "--pos-animation-shift",
+        `${shiftAmount}px`
+      );
+      document.body.style.setProperty(
+        "--neg-animation-shift",
+        `-${shiftAmount}px`
+      );
+    }
+  }, [market]);
   return (
     <>
       <div className="Navbar">
@@ -54,6 +106,15 @@ function Navbar() {
           <img src={logo} onClick={() => navigate("/")} alt="Cash logo" />
         </div>
         {navbutton}
+      </div>
+      <div className="Market">
+        <div ref={marketRef} className="Market-container">
+          {market.map((ticker, i) => (
+            <p key={i}>
+              {ticker.name} : <span> {ticker.price}</span>
+            </p>
+          ))}
+        </div>
       </div>
     </>
   );
